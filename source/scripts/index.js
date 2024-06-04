036d1a8e494a0a1504e0bd87ac8ad6bed81f321d
Elena Swiderskaya <olenyonock7@rambler.ru>
2024-06-04 22:04:48 +0300

    Добавляет детализацию верстки и интерактивную карту  (#8)
    * Добавляет детализацию главной странице
    
    * Добавляет детализацию странице каталога
    
    * Добавляет no-js поведение
    
    * Добавляет интерактивную карту
    
    * Добавляет правки блока hero
    
    * Переносит стили карты в стилевой файл
    
    * Добавляет оптимизацию верстки
    
    * Добавляет дата-атрибуты для тестирования
    
    * Добавляет ссылки к карточкам товаров

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,17 @@
 /* в этот файл добавляет скрипты*/
+// disable no-js
+const header = document.querySelector('.header');
+header.classList.remove('header--no-js');
+
+const navigation = document.querySelector('.navigation__list');
+navigation.classList.remove('navigation__list--no-js');
+
+const map = document.querySelector('.map__interactive-map');
+map.classList.remove('map__interactive-map--no-js');
+
+// toggle-menu
+const menu = document.querySelector('.navigation__menu-button');
+menu.addEventListener('click', () => {
+  menu.classList.toggle('navigation__menu-button--open');
+  navigation.classList.toggle('navigation__list--open');
+});
